8cf5c7ce122467811af5a2a3b51e2c31cb73d894
Fadzlan <horizonmyto@gmail.com>
2023-12-26 17:46:08 +0800

    hai 6

--- main.js
+++ main.js
@@ -93,6 +93,8 @@
  *     summary: Register a new visitor
  *     tags: 
  *       - User Management
+ *     security:
+ *       - BearerAuth: []
  *     description: Endpoint to create a new visitor record.
  *     requestBody:
  *       description: Visitor information
