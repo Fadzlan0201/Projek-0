098050bf98e3dba516947eeed3d9542c2cb49fd2
Fadzlan <horizonmyto@gmail.com>
2024-01-05 23:38:36 +0800

    hai 8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-//const port = process.env.PORT || 3000;
-const port =  3000;
+const port = process.env.PORT || 3000;
+//const port =  3000;
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const MongoURI = process.env.MONGODB_URI
